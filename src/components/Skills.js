bc8c2705ac65dd0fb9b9f2e029f096212f8e6725
Giuxpro <albertogiusepp@gmail.com>
2022-09-27 15:19:49 -0300

    skills text and some integratios

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -78,23 +78,77 @@ export const Skills = () => {
                         <h2>
                           Technical Skills
                         </h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        <p>The only long-term competitive ability, is the ability to learn, but this alone becomes nothing without the opportunity</p>
                         <Carousel responsive={responsive} infinite={true} className="skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Img1"/>
-                                <h5>Web Developer</h5>
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://www.w3schools.com/html/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/732/732212.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>HTML</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Img2"/>
-                                <h5>Web Developer</h5>
+                                {/* <img src={meter2} alt="Img2"/> */}
+                                <a href="https://www.w3schools.com/css/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/732/732190.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>CSS</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Img3"/>
-                                <h5>Web Developer</h5>
+                                {/* <img src={meter3} alt="Img3"/> */}
+                                <a href="https://www.javascript.com/" target="_blank"><img src="https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>JavaScript</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Img1"/>
-                                <h5>Web Developer</h5>
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://www.typescriptlang.org/" target="_blank"><img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/typescript_original_logo_icon_146317.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>TypeScript</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://reactjs.org/" target="_blank"><img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>React.JS</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://redux.js.org/" target="_blank"><img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/redux-icon.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>Redux</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://nodejs.org/" target="_blank"><img src="https://img.icons8.com/color/480/nodejs.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>Node.JS</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://expressjs.com/" target="_blank"><img src="https://kalvanaveen.github.io/WebDevelopmentResources.github.io/Images/Express-JS-min.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>Express.JS</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://sequelize.org/" target="_blank"><img width="40px" src="https://s2.qwant.com/thumbr/0x380/f/1/def6e5a6cedacd5856251aeaef7e52119bf19a4f70ada987080f4a3db8e074/sequelize-logo-png-transparent.png?u=https%3A%2F%2Fcdn.freebiesupply.com%2Flogos%2Flarge%2F2x%2Fsequelize-logo-png-transparent.png&q=0&b=1&p=0&a=0" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>Sequelize</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://www.postgresql.org/" target="_blank"><img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/postgresql_original_logo_icon_146391.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>PostgreSql</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://www.mongodb.com/" target="_blank"><img src="https://stickersllamita.com/wp-content/uploads/2022/02/MongoDB.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>MongoDB</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://getbootstrap.com/" target="_blank"><img src="https://icons.getbootstrap.com/assets/img/icons-hero.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>Bootstrap</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://github.com/" target="_blank"><img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>Github</h5>
+                            </div>
+                            <div className="item">
+                                {/* <img src={meter1} alt="Img1"/> */}
+                                <a href="https://wordpress.com/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/174/174881.png" style={{height:"100px", width:"100px"}}/></a>
+                                <h5>WordPress</h5>
                             </div>
                             
                         </Carousel>
